8ce4b308ce814fa7309f6e5a71319f770606260a
jeongminji <parapin2274@gmail.com>
2025-07-03 21:10:26 +0900

    [Docs] Xcode 파일 추가

---
+++ SwiftUI-Workbook-practice/SwiftUI-Workbook-practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Workbook-practice
+//
+//  Created by jeongminji on 7/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Workbook-practice/SwiftUI-Workbook-practice/SwiftUI_Workbook_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Workbook_practiceApp.swift
+//  SwiftUI-Workbook-practice
+//
+//  Created by jeongminji on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Workbook_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
