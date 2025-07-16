34f6822b77332204b9267b985af8b5df0a08d181
jeongminji <parapin2274@gmail.com>
2025-07-16 22:58:02 +0900

    [Feat] #3 - Model 작성하기

--- SwiftUI-Workbook-practice/SwiftUI-Workbook-practice/Week3/RainbowModel.swift
+++ SwiftUI-Workbook-practice/SwiftUI-Workbook-practice/Week3/RainbowModel.swift
@@ -9,5 +9,51 @@ import Foundation
 import SwiftUI
 
 enum RainbowModel: CaseIterable {
+    case red
+    case orange
+    case yellow
+    case green
+    case blue
+    case indigo
+    case purple
     
+    /// case에 해당하는 색을 반환합니다.
+    /// - Returns: 지정된 색 반환
+    func returnColor() -> Color {
+        switch self {
+        case .red:
+            return Color.rainbowRed
+        case .orange:
+            return Color.rainbowOrange
+        case .yellow:
+            return Color.rainbowYellow
+        case .green:
+            return Color.rainbowGreen
+        case .blue:
+            return Color.rainbowBlue
+        case .indigo:
+            return Color.rainbowIndigo
+        case .purple:
+            return Color.rainbowPurple
+        }
+    }
+    
+    func returnColorName() -> String {
+        switch self {
+        case .red:
+            return "빨강"
+        case .orange:
+            return "주황"
+        case .yellow:
+            return "노랑"
+        case .green:
+            return "초록"
+        case .blue:
+            return "파랑"
+        case .indigo:
+            return "남색"
+        case .purple:
+            return "보라"
+        }
+    }
 }
